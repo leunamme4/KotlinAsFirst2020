0ac644128235260d29c0af7f61cc2bd4c854b56c
Flay98 <jancovalenko@yandex.ru>
2021-09-30 20:16:37 +0300

    full Lesson 2 except 2 exercises from Logical.

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -18,8 +18,9 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
-
+fun isNumberHappy(number: Int): Boolean {
+    return (number / 1000 + (number / 100) % 10) == ((number / 10) % 10 + number % 10)
+}
 /**
  * Простая (2 балла)
  *
@@ -27,8 +28,9 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
-
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
+    return (x1 == x2) || (y1 == y2) || ((x1 - x2) * (x1 - x2) == ((y1 - y2) * (y1 - y2)))
+}
 
 /**
  * Простая (2 балла)
@@ -59,4 +61,11 @@ fun circleInside(
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
+    return when {
+        ((a <= r) and (b <= s)) or ((a <= s) and (b <= r)) -> true
+        ((a <= r) and (c <= s)) or ((a <= s) and (c <= r)) -> true
+        ((c <= r) and (b <= s)) or ((c <= s) and (b <= r)) -> true
+        else -> false
+    }
+}
